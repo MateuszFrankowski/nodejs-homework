d6bdfc1c69e02c37e9f20cb904deb7dad8c71602
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2023-04-11 23:13:32 +0200

    hw-2 finished

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,76 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join("./models", "contacts.json");
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: "utf8" });
+    return JSON.parse(data);
+  } catch (err) {
+    console.err(err);
+  }
+};
 
-const listContacts = async () => {}
+const getContactById = async (contactId) => {
+  const data = await listContacts();
+  return data.find((contact) => contactId === contact.id);
+};
 
-const getContactById = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const data = await listContacts();
+  const contactToDelete = data.find((contact) => contactId === contact.id);
+  const newData = data.filter((contact) => contactId !== contact.id);
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(newData));
+  } catch (err) {
+    console.err(err);
+  }
+  return contactToDelete;
+};
 
-const removeContact = async (contactId) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const newContact = { id: nanoid(), ...body };
+  const data = await listContacts();
+  const newData = [...data, newContact];
+  let err;
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(newData));
+  } catch (err) {
+    console.error(err);
+  }
+  return { newContact, err };
+};
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  const newContact = { id: nanoid(), ...body };
+  const data = await listContacts();
 
-const addContact = async (body) => {}
+  const contactToUpdate = data.find((contact) => contact.id === contactId);
+  if (!contactToUpdate) return;
+  if (email) {
+    contactToUpdate.email = email;
+  }
+  if (name) {
+    contactToUpdate.name = name;
+  }
+  if (phone) {
+    contactToUpdate.phone = phone;
+  }
 
-const updateContact = async (contactId, body) => {}
+  const updatedData = data.map((contact) =>
+    contact.id === contactId ? { ...contact, ...contactToUpdate } : contact
+  );
+
+  let err;
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(updatedData));
+  } catch (err) {
+    console.error(err);
+  }
+
+  return { contactToUpdate, err };
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +78,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,81 @@
-const express = require('express')
+const express = require("express");
+const contactsHandler = require("../../models/contacts.js");
+const router = express.Router();
+const Joi = require("joi");
+// const app = express();
+// app.use(express.json());
+router.get("/", async (req, res, next) => {
+  const allContacts = await contactsHandler.listContacts();
+  res.json({ allContacts: allContacts });
+});
 
-const router = express.Router()
+router.get("/:contactId", async (req, res, next) => {
+  const contactById = await contactsHandler.getContactById(
+    req.params.contactId
+  );
+  if (!contactById) return res.sendStatus(404);
+  res.json({ contactById: contactById });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string()
+      .regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+      .messages({
+        "string.pattern.base": `Phone number must have at least 7 digits.`,
+      })
+      .required(),
+  });
+  const { error } = schema.validate({
+    name: name,
+    email: email,
+    phone: phone,
+  });
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (error) return res.status(400).json(error.details[0].message);
+  const { newContact, err } = contactsHandler.addContact(req.body);
+  if (err) return res.status(500).json(err);
+  return res.status(201).json(newContact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const removeContactById = await contactsHandler.removeContact(
+    req.params.contactId
+  );
+  if (!removeContactById) return res.sendStatus(404);
+  res.status(200).json({ removedContactById: removeContactById });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const { name, email, phone } = req.body;
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30),
+    email: Joi.string().email(),
+    phone: Joi.string()
+      .regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+      .messages({
+        "string.pattern.base": `Phone number must have at least 7 digits.`,
+      }),
+  }).or(" name", " email", "phone");
+  const { error } = schema.validate({
+    name: name,
+    email: email,
+    phone: phone,
+  });
 
-module.exports = router
+  if (error) return res.status(400).json({ message: error.details[0].message });
+  const { contactToUpdate, err } = await contactsHandler.updateContact(
+    req.params.contactId,
+    req.body
+  );
+
+  if (err) return res.sendStatus(500);
+  if (!contactToUpdate) return res.sendStatus(404);
+  res.status(200).json({ contactUpdated: contactToUpdate });
+});
+
+module.exports = router;
